f9d0f0dd28053d1f6efbdace8d70c987e8e4cb38
TinaLoh <tinalola@gmail.com>
2014-12-23 14:26:50 -0800

    returns title

---
+++ lib/poem_parser.rb
@@ -0,0 +1,15 @@
+class PoemParser
+
+  def initialize(poem)
+      @poem = poem
+  end
+
+  def title
+    @poem.lines.first.chomp
+  end
+
+  def author
+    poem_lines = @poem.lines
+    poem_lines[1].strip
+
+end
